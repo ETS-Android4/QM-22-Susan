f3803c051ceda2bf17a223797a502f8f235544c7
cnieh49 <calebnieh@gmail.com>
2021-11-10 09:50:51 -0500

    Start of Auto
    shit isn't getting done :)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/RedSimpleAuto.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.Auto;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.ClassFactory;
+import org.firstinspires.ftc.robotcore.external.Func;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
+import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
+import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
+import org.firstinspires.ftc.teamcode.simpleBotCode.HardwareSimpleBot;
+
+import java.util.List;
+import java.util.Locale;
+
+@Autonomous(name = "Red Simple Auto", gr3oup = "!Primary")
