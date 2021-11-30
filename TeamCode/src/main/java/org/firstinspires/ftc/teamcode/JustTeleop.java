39d01ca12127ffdce0065527eab47c94b3b8af1c
cnieh49 <calebnieh@gmail.com>
2021-11-30 17:01:55 -0500

    Update JustTeleop.java
    Changed code slightly - should drive straight now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JustTeleop.java
@@ -10,7 +10,7 @@ import com.qualcomm.robotcore.util.Range;
 
 
 @TeleOp
-    public class JustTeleop extends OpMode {
+public class JustTeleop extends OpMode {
     DcMotor RFmotor;
     DcMotor RBmotor;
     DcMotor LFmotor;
@@ -51,21 +51,15 @@ import com.qualcomm.robotcore.util.Range;
     @Override
     public void loop() {
 
-            //turntable
-            if(gamepad2.a){
-                double turnTablePower=0.2;
-                turnTable.setPower(turnTablePower);
-            telemetry.addData("A button","Pressed");
-            } else{
-                turnTable.setPower(0.0);}
+        //turntable
+        if(gamepad2.a){
+            double turnTablePower=0.2;
+            turnTable.setPower(turnTablePower);
+        telemetry.addData("A button","Pressed");
+        } else{
+            turnTable.setPower(0.0);}
 
-
-
-
-
-
-
-//Slider
+        //Slider
 
         if (gamepad2.left_trigger > 0.1)
             sliderSpool.setPower(-1);
@@ -73,7 +67,8 @@ import com.qualcomm.robotcore.util.Range;
             sliderSpool.setPower(.5);
         else
             sliderSpool.setPower(0);
-//Intake
+
+        //Intake
         if (gamepad2.right_trigger > 0.1)
             intakeMotor.setPower(gamepad2.right_trigger);
         if (gamepad2.right_bumper)
@@ -84,17 +79,15 @@ import com.qualcomm.robotcore.util.Range;
         //Init variables
 
 
-
 //
 //Drive Controls:
 // Left stick= Translational Movement
 // Right Stick= Rotational Movement
         //DRIVE_STICK_THRESHOLD = deadzone
 
-
-        double Turn = gamepad1.left_stick_y;
-        double Speed = gamepad1.right_stick_x;
-        double Strafe= -gamepad1.left_stick_x;
+        double Speed = -gamepad1.left_stick_y;
+        double Turn = gamepad1.right_stick_x;
+        double Strafe = gamepad1.left_stick_x;
         double MAX_SPEED = 1.0;
 
         double frontLeftPower;
@@ -105,17 +98,17 @@ import com.qualcomm.robotcore.util.Range;
 
 
 
-   if (Speed < -DRIVE_STICK_THRESHOLD || Speed > DRIVE_STICK_THRESHOLD || Turn < -DRIVE_STICK_THRESHOLD || Turn > DRIVE_STICK_THRESHOLD || Strafe < -DRIVE_STICK_THRESHOLD || Strafe > DRIVE_STICK_THRESHOLD){
+       if (Speed < -DRIVE_STICK_THRESHOLD || Speed > DRIVE_STICK_THRESHOLD || Turn < -DRIVE_STICK_THRESHOLD || Turn > DRIVE_STICK_THRESHOLD || Strafe < -DRIVE_STICK_THRESHOLD || Strafe > DRIVE_STICK_THRESHOLD){
             rearRightPower = Range.clip(Speed - Turn - Strafe, -1, 1);
             rearLeftPower= Range.clip(Speed + Turn - Strafe,-1, 1);
             frontRightPower= Range.clip(Speed + Turn - Strafe, -1, 1);
             frontLeftPower = Range.clip(Speed + Turn + Strafe, -1, 1);
 
-       telemetry.addData("Front-right motor", "%5.2f", frontRightPower);
-       telemetry.addData("Back-right motor", "%5.2f", rearRightPower);
-       telemetry.addData("Front-left motor", "%5.2f", frontLeftPower);
-       telemetry.addData("Back-left motor", "%5.2f", rearLeftPower);
-       telemetry.update();
+           telemetry.addData("Front-right motor", "%5.2f", frontRightPower);
+           telemetry.addData("Back-right motor", "%5.2f", rearRightPower);
+           telemetry.addData("Front-left motor", "%5.2f", frontLeftPower);
+           telemetry.addData("Back-left motor", "%5.2f", rearLeftPower);
+           telemetry.update();
 
             RBmotor.setPower(rearRightPower);
             RFmotor.setPower(frontRightPower);
@@ -123,14 +116,15 @@ import com.qualcomm.robotcore.util.Range;
             LFmotor.setPower(frontLeftPower);
 
 
-        }
-        else
-            RBmotor.setPower(0);
-        RFmotor.setPower(0);
-        LBmotor.setPower(0);
-        LFmotor.setPower(0);
+       }
+       else {
+           RBmotor.setPower(0);
+           RFmotor.setPower(0);
+           LBmotor.setPower(0);
+           LFmotor.setPower(0);
+       }
     }
-    }
+}
 
 
 
