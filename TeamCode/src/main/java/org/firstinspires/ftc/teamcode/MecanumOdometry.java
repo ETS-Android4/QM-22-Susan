3f21aa77c11610ce676751f3a2df10368a993407
cnieh49 <calebnieh@gmail.com>
2021-12-01 03:03:41 -0500

    TensorFlow added for camera Hardware functions added (will probably need to change some of them) Autonomous file set up

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumOdometry.java
@@ -14,10 +14,114 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import org.firstinspires.ftc.robotcontroller.external.samples.HardwarePushbot;
-
+import org.firstinspires.ftc.teamcode.IMU;
 
 @Autonomous
-public class MecanumOdometry extends LinearOpMode {
+public class MecanumOdometry{// extends LinearOpMode {
+    // Encoder constants
+    public static final double CM_PER_TICK_A = -100.0 / 1673.0;
+    public static final double CM_PER_TICK_B = -100.0 / 1693.0;
+    public static final double CM_PER_TICK_C = -100.0 / 1434.0;
+    // This is theoretically correct, but may need to be changed in practice
+    public static final double STRAFE_CONSTANT = .5;
+
+    // The current position and orientation of the robot
+    private double x = 0;
+    private double y = 0;
+    private double theta = 0;
+
+    // The angle at which the robot was facing when the OpMode started
+    public float startingAngle = 0;
+
+    // The previous encoder values
+    private double oldA = 0;
+    private double oldB = 0;
+    private double oldC = 0;
+
+    private double dx = 0;
+    private double dy = 0;
+    // The IMU handler - tracks the robot's orientation
+    public IMU imu = new IMU();
+
+    /**
+     * Should be called in when the OpMode is initialized
+     */
+    public void init(HardwareMap hardwareMap) {
+        imu.initIMU(hardwareMap);
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public double getTheta() {
+        return theta;
+    }
+
+    /**
+     * Should be called when the OpMode is started
+     * Once again
+     *
+     * @param a The current front-right encoder value
+     * @param b The current front-left encoder value
+     * @param c The current back-left encoder value
+     */
+    public void start(double a, double b, double c) {
+        imu.update();
+        startingAngle = imu.getZAngle();
+        x = 0;
+        y = 0;
+        oldA = a;
+        oldB = b;
+        oldC = c;
+    }
+
+    /**
+     * Updates the position and orientation based on new encoder values and the IMU
+     *
+     * @param a The current front-right encoder value
+     * @param b The current front-left encoder value
+     * @param c The current back-left encoder value
+     */
+    public void update(double a, double b, double c) {
+        // Calculate the differences between the new and old encoder values
+        // and convert them to centimeters.
+        double dA = (a - oldA) * CM_PER_TICK_A;
+        double dB = (b - oldB) * CM_PER_TICK_B;
+        double dC = (c - oldC) * CM_PER_TICK_C;
+
+
+        oldA = a;
+        oldB = b;
+        oldC = c;
+
+        // Update the orientation using data from the IMU
+        imu.update();
+        theta = imu.getZAngle() - startingAngle;
+
+        // Update the total displacement using the orientation and encoder displacements
+        double averageEncoderChange = (Math.abs(dB) + Math.abs(dC)) / 2;
+        if (dB > 0 && dC < 0) {
+            dx = averageEncoderChange * STRAFE_CONSTANT * Math.cos(theta);
+            dy = averageEncoderChange * STRAFE_CONSTANT * Math.sin(theta);
+        } else if (dB < 0 && dC > 0) {
+            dx = -averageEncoderChange * STRAFE_CONSTANT * Math.cos(theta);
+            dy = -averageEncoderChange * STRAFE_CONSTANT * Math.sin(theta);
+        } else {
+            dx = (dA + dB) / 2 * Math.sin(theta);
+            dy = (dA + dB) / 2 * Math.cos(theta);
+        }
+
+        // Add the displacement to the total position
+        this.x += dx;
+        this.y += dy;
+    }
+
+    /*
     DcMotor RFmotor;
     DcMotor RBmotor;
     DcMotor LFmotor;
@@ -37,11 +141,9 @@ public class MecanumOdometry extends LinearOpMode {
     public static int slideLevelTwo= 537*10;
     public static int slideLevelThree= 537*20;
 
+
     @Override
     public void runOpMode() throws InterruptedException {
-
-
-
 
         RFmotor = hardwareMap.get(DcMotor.class, "rightfront");
         RBmotor = hardwareMap.get(DcMotor.class, "rightback");
@@ -58,8 +160,8 @@ public class MecanumOdometry extends LinearOpMode {
         LFmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
         LBmotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
-       sliderSpool.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-       intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        sliderSpool.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        intakeMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
 
         RFmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
@@ -138,85 +240,7 @@ public class MecanumOdometry extends LinearOpMode {
        LBmotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
     }
-
-
-    /**
-     * Should be called in when the OpMode is initialized
-     */
-
-
-
-
-
-
-
-
-
-    //public void init(HardwareMap hardwareMap) { imu.initIMU(hardwareMap); }
-
-//    public double getX() {
-//        return x;
-//    }
-//
-//    public double getY() {
-//        return y;
-//    }
-//
-//    public double getTheta() {
-//        return theta;
-//    }
-
-    /**
-     * Should be called when the OpMode is started
-     * Once again
-     *
-     * @param a The current front-right encoder value
-     * @param b The current front-left encoder value
-     * @param c The current back-left encoder value
-     */
-
-
-    /**
-     * Updates the position and orientation based on new encoder values and the IMU
-     *
-     * @param a The current front-right encoder value
-     * @param b The current front-left encoder value
-     * @param c The current back-left encoder value
-     */
-
-
-//    public void update(double a, double b, double c) {
-//        // Calculate the differences between the new and old encoder values
-//        // and convert them to centimeters.
-//        double dA = (a - oldA) * CM_PER_TICK_A;
-//        double dB = (b - oldB) * CM_PER_TICK_B;
-//        double dC = (c - oldC) * CM_PER_TICK_C;
-//
-//
-//        oldA = a;
-//        oldB = b;
-//        oldC = c;
-//
-//
-//        // Update the total displacement using the orientation and encoder displacements
-//        double averageEncoderChange = (Math.abs(dB) + Math.abs(dC)) / 2;
-//        if (dB > 0 && dC < 0) {
-//            dx = averageEncoderChange * STRAFE_CONSTANT * Math.cos(theta);
-//            dy = averageEncoderChange * STRAFE_CONSTANT * Math.sin(theta);
-//        } else if (dB < 0 && dC > 0) {
-//            dx = -averageEncoderChange * STRAFE_CONSTANT * Math.cos(theta);
-//            dy = -averageEncoderChange * STRAFE_CONSTANT * Math.sin(theta);
-//        } else {
-//            dx = (dA + dB) / 2 * Math.sin(theta);
-//            dy = (dA + dB) / 2 * Math.cos(theta);
-//        }
-//
-//        // Add the displacement to the total position
-//        this.x += dx;
-//        this.y += dy;
-//
-//
-//
+    */
     }
 
 
